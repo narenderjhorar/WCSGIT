a9b202f14b0990709e85d4602cadefc51900754b
wcsadmin <wcsadmin@Vanila-WCS7.localdomain>
2019-11-07 12:50:34 +0530

    commit for dev branch

--- P2WCS/src/p2pkg/p2file1.java
+++ P2WCS/src/p2pkg/p2file1.java
@@ -4,5 +4,6 @@ public class p2file1 {
 public static void main(String[] args) {
 	System.out.println("p2file1");
 	System.out.println("p2file1 added from github");
+	System.out.println("p2file1 added for branch development");
 }
 }
