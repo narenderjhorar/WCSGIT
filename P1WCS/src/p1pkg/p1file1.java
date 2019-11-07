d46020e2a8938b5f7997bcde792eb4902639f066
wcsadmin <wcsadmin@Vanila-WCS7.localdomain>
2019-11-07 13:56:15 +0530

    p1file1 added from github01

--- P1WCS/src/p1pkg/p1file1.java
+++ P1WCS/src/p1pkg/p1file1.java
@@ -4,6 +4,6 @@ public class p1file1 {
  public static void main(String[] args) {
 	System.out.println("p1file1");
 	System.out.println("p1file1 added from github");
-	 System.out.println("p1file1 added from github111");
+	System.out.println("p1file1 added from github01");
 }
 }
