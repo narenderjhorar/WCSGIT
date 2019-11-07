e7c7932cce2f717e557d2ea11592527e2ffd4ac7
narenderjhorar <54709024+narenderjhorar@users.noreply.github.com>
2019-11-07 13:07:27 +0530

    Update p2file1.java
    github branch development

--- P2WCS/src/p2pkg/p2file1.java
+++ P2WCS/src/p2pkg/p2file1.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("p2file1");
 	System.out.println("p2file1 added from github");
 	System.out.println("p2file1 added for branch development");
+	System.out.println("p2file1 added from github branch development");
 }
 }
